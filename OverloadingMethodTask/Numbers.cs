abe7517d5dac1a147c44d681b068495c0db3298e
HardyWisnu <hardywisnu@amikom.students.ac.id>
2022-04-07 21:34:03 +0700

    TugasMethod

---
+++ OverloadingMethodTask/Numbers.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingMethodTask
+{
+    public class Numbers
+    {
+        public int FindMinimum(int number1, int number2)
+        {
+            if(number1 < number2)
+            {
+                return (number1);
+            }
+            else if(number2 < number1)
+            {
+                return number2;
+            }
+            return 0;
+        }
+        public int FindMinimum(int number1, int number2, int number3)
+        {
+            if (number1 < number2 && number1 < number3)
+            {
+                return number1;
+            }
+            else if (number2 < number1 && number2 < number3)
+            {
+                return number2;
+            }
+            else if (number3 < number1 && number3 < number2)
+            {
+                return number3;
+            }
+            return 0;
+
+        }
+        public int FindMaximum(int number1, int number2)
+        {
+            if (number1 > number2)
+            {
+                return number1;
+            }
+            else if (number2 > number1)
+            {
+                return number2;
+            }
+            return 0;
+        }
+        public int FindMaximum(int number1, int number2, int number3)
+        {
+            if (number1 > number2 && number1 > number3)
+            {
+                return number1;
+            }
+            else if (number2 > number1 && number2 > number3)
+            {
+                return number2;
+            }
+            else if (number3 > number1 && number3 > number2)
+            {
+                return number3;
+            }
+            return 0;
+        }
+    }
+}
